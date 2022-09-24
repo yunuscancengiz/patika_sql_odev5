0a6cdd7259212e229ba8d5084e16e66e83417597
yunuscancengiz <90009985+yunuscancengiz@users.noreply.github.com>
2022-09-24 22:37:51 +0300

    Create patika_odev5.sql

---
+++ patika_odev5.sql
@@ -0,0 +1,27 @@
+-- Merhabalar,
+
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-- 1) film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+-- 2) film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+-- 3) customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+-- Kolay Gelsin.
+
+-- 1.SORU
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+-- 2. SORU
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+-- 3. SORU
+SELECT store_id, first_name, last_name FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
